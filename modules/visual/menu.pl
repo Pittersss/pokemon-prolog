96cf5d02cd32eecfd6398cb256335b760f99b3e3
Pedro Malaquias <pedromalaquias608@gmail.com>
2025-03-31 13:33:06 -0300

    fix(menu.pl)

--- modules/visual/menu.pl
+++ modules/visual/menu.pl
@@ -1,12 +1,20 @@
-:- module(menu, [create_menu/0]). %Can be exported declaration
+:- module(menu, [create_menu/0]).
 :- use_module(library(pce)).
 
-create_menu:-
+create_menu :-
     new(Window, frame('Pokemon Prolog')),
-    new(Button, button('Start', message(@prolog, open_choicePk))),
     new(Dialog, dialog),
-    send(Dialog, append, Button),
-    send(Button, alignment, center), 
+    new(Button, button('Jogar', message(@prolog, open_choicePk))),
+
+    send(Dialog, size, size(300, 100)),  
+
+    new(Box, box(345, 100)), 
+    send(Box, pen, 0),
+
+    send(Dialog, append, Box),
+    send(Dialog, append, Button, below),
+    send(Dialog, layout),
+
     send(Window, append, Dialog),
     send(Window, open),
     send(Window, size, size(400, 300)),
