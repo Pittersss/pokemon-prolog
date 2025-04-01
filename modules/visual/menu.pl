bc17fb8df428aed26e0a6fb474aa1a73491f5c82
=Pittersss <pedromalaquias608@gmail.com>
2025-04-01 15:29:10 -0300

    feat(dynamic window)

--- modules/visual/menu.pl
+++ modules/visual/menu.pl
@@ -4,20 +4,21 @@
 create_menu :-
     new(Window, frame('Pokemon Prolog')),
     new(Dialog, dialog),
-    new(Button, button('Jogar', message(@prolog, open_choicePk))),
-
+    new(Button, button('Jogar', message(@prolog, open_choicePk, Window))),
     send(Dialog, size, size(300, 100)),  
-
     new(Box, box(345, 100)), 
     send(Box, pen, 0),
-
     send(Dialog, append, Box),
     send(Dialog, append, Button, below),
     send(Dialog, layout),
-
     send(Window, append, Dialog),
     send(Window, open),
     send(Window, size, size(400, 300)),
     send(Window, position, point(600, 150)).
 
-open_choicePk :- send(@display, inform, 'Botão clicado!').
+open_choicePk(OldWindow) :-
+    send(OldWindow, destroy),
+    new(NewWindow, frame('Escolha seu Pokémon')),
+    send(NewWindow, open),
+    send(NewWindow, size, size(400, 300)),
+    send(NewWindow, position, point(600, 150)).
